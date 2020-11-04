d9457d500ab2fb9e12fe3e6bb4aa9be9bd05117c
AnastasiyaKukharska <70960052+AnastasiyaKukharska@users.noreply.github.com>
2020-11-04 02:37:37 +0200

    Add files via upload

---
+++ BL.Tests/AuthManagerTests.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLogic;
+using DAL.Interfaces;
+using DTO;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Tests
+{
+    [TestFixture]
+    public class AuthManagerTests
+    {
+        private Mock<IUserDal> userDal;
+        private Mock<IBasketDal> basketDal;
+        private Mock<ICategoriesDal> categoryDal;
+        private Mock<IBooksDal> bookDal;
+        private AuthManager manager;
+
+        [SetUp]
+        public void Setup()
+        {
+            userDal = new Mock<IUserDal>(MockBehavior.Strict);
+            basketDal= new Mock<IBasketDal>(MockBehavior.Strict);
+            categoryDal = new Mock<ICategoriesDal>(MockBehavior.Strict);
+            bookDal = new Mock<IBooksDal>(MockBehavior.Strict);
+            manager = new AuthManager(userDal.Object, basketDal.Object, categoryDal.Object,  bookDal.Object);
+        }
+
+
+        [Test]
+        public void LoginUserTest()
+        {
+            string username = "user";
+            string password = "pass";
+
+            userDal.Setup(d => d.Login(username, password)).Returns(true);
+            var res = manager.Login(username, password);
+
+            Assert.IsTrue(res);
+        }
+    }
+}
---
+++ BL.Tests/BLTest.cs
@@ -0,0 +1,61 @@
+﻿using DAL.Interfaces;
+using DAL.Concrete;
+using DTO;
+using NUnit.Framework;
+using System;
+using System.Configuration;
+
+using System.EnterpriseServices;
+
+using System.Runtime.InteropServices;
+
+
+namespace BusinessLogic
+{
+
+    [TestFixture]
+    [Transaction(TransactionOption.RequiresNew), ComVisible(true)]
+    public class BLTest
+    {
+
+
+
+        [Test]
+        public void AddBasketTest()
+        {
+            // BasketDal dal = new BasketDal(ConfigurationManager.ConnectionStrings["IMDB"].ConnectionString);
+            var b = new BasketDTO
+            {
+                Title = "BLTest for basket",
+                UserID = 1,
+                BookID = 1,
+                StatusID = 1,
+                Author = "BLTest one",
+                Price = 100,
+                Amount = 1,
+                Date = DateTime.Now
+            };
+            var result = new BasketDTO();
+            result.AddBasket(b);
+            Assert.IsTrue(result.BasketID != 0, "returned ID should be more than zero");
+
+        }
+        [TearDown]
+        public void Teardown()
+        {
+            if (ContextUtil.IsInTransaction)
+            {
+                ContextUtil.SetAbort();
+            }
+        }
+
+        [Test]
+        public void GetBasketTest()
+        {
+            BasketDAL dal = new BasketDAL(ConfigurationManager.ConnectionStrings["IMDB"].ConnectionString);
+            BasketDTO result = new BasketDTO(dal);
+
+            Assert.IsTrue(result.GetBasket(5).BasketID == 5, "Cannot find the item");
+        }
+    }
+}
