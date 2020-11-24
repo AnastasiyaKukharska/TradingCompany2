fb5ea4c1303c2c9b30f9395d816948620cc0f46a
AnastasiyaKukharska <70960052+AnastasiyaKukharska@users.noreply.github.com>
2020-11-24 23:04:41 +0200

    Add files via upload

---
+++ DTA/IBooksDal.cs
@@ -0,0 +1,18 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IBooksDal
+    {
+        BooksDTO GetBookById(int id);
+        List<BooksDTO> GetAllBooks();
+        BooksDTO UpdateMovie(BooksDTO book);
+        BooksDTO CreateMovie(BooksDTO book);
+        void DeleteBook(int id);
+    }
+}
