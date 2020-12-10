2d9bf6db19de29d38a37f7060f6532dec9defa8e
AnastasiyaKukharska <70960052+AnastasiyaKukharska@users.noreply.github.com>
2020-12-10 16:26:27 +0200

    Add files via upload

---
+++ MVC2/App/Security/CustomMembershipUser.cs
@@ -0,0 +1,31 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace MVC2.App.Security
+{
+    public class CustomMembershipUser : MembershipUser
+    {
+        public int UserId { get; set; }
+        public ICollection<PrivilegeDTO> Roles { get; set; }
+
+        public CustomMembershipUser(UserDTO user)
+            : base("CustomMembership",
+                  user.Login,
+                  user.UserID,
+                  string.Empty,
+                  string.Empty,
+                  true, false,
+                  DateTime.Now,
+                  DateTime.Now,
+                  DateTime.Now,
+                  DateTime.Now)
+        {
+            UserId = user.UserID;
+            Roles = user.Privileges;
+        }
+    }
+}
---
+++ MVC2/App/Security/CustomSerializeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC2.App.Security
+{
+    public class CustomSerializeModel
+    {
+        public int UserID { get; set; }
+        public string Login { get; set; }
+        public List<string> Roles { get; set; }
+    }
+}
